9ac9f649d0c2da636a6dbea1d3955a80c98de8fe
entitysport <manojbyweb@gmail.com>
2019-07-12 16:38:58 +0530

    Update get_all_functions.js

--- get_all_functions.js
+++ get_all_functions.js
@@ -12,9 +12,9 @@ module.exports = function (api) {
 
     get_seasons_data(sid=0,args={}, callback) {
       if(sid){
-        let path = 'season/'+sid+'/competitions';
+        var path = 'season/'+sid+'/competitions';
       }else{
-        let path = 'seasons';
+        var path = 'seasons';
       }
       return api.get({
           url: path,
@@ -24,9 +24,9 @@ module.exports = function (api) {
 
     get_competitions_data(cid=0,args={}, callback) {
         if(cid){
-          let path = 'competition/'+cid;
+          var path = 'competition/'+cid;
         }else{
-          let path = 'competitions';
+          var path = 'competitions';
         }
         return api.get({
             url: path,
@@ -60,9 +60,9 @@ module.exports = function (api) {
 
     get_matches_data(mid=0,args={}, callback) {
       if(mid){
-        let path = 'matches/'+mid+'/info';
+        var path = 'matches/'+mid+'/info';
       }else{
-        let path = 'matches';
+        var path = 'matches';
       }
       return api.get({
           url: path,
@@ -88,9 +88,9 @@ module.exports = function (api) {
 
     get_teams_data(tid=0,args={}, callback) {
       if(tid){
-        let path = 'team/'+tid+'/info';
+        var path = 'team/'+tid+'/info';
       }else{
-        let path = 'teams';
+        var path = 'teams';
       }
       return api.get({
           url: path,
@@ -108,9 +108,9 @@ module.exports = function (api) {
 
     get_players_data(pid=0,args={}, callback) {
       if(pid){
-        let path = 'player/'+pid+'/profile';
+        var path = 'player/'+pid+'/profile';
       }else{
-        let path = 'players';
+        var path = 'players';
       }
       return api.get({
           url: path,
